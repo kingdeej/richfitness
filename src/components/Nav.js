788563a99c49a66c9227d41c82f1e0ec022382df
kingdeej <devonhenry10196@gmail.com>
2023-05-27 14:48:00 -0500

    main

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import navIcon from '../images/icons/nav-icon.svg'
 
 function Nav() {
+  const [hide, setHide] = useState('')
+  const [active, setActive] = useState('deactivate')
+  function toggle(){
+    if (hide === 'hide') {
+      setHide('')
+    }else{
+      setHide('') 
+    }    
+    if (active === 'deactivate') {
+      setActive('activate')
+    }
+    if (active === 'activate') {
+      setActive('deactivate')
+    }
+  }
   return (
     <nav>
         <div className="nav-wrapper || page-inline-padding space-between vertical-align">
             <a href='/' className="logo">RICH FITNESS</a>
             <div className="right-nav || ">
-              .nav
-              <img src="" className='nav-icon' alt="" />
-                <ul className="nav-links || flex ">
+              <div className={`nav-icon-cont  ${active}`} onClick={toggle}>
+                <img src={navIcon} id='nav-icon-1' className={`nav-icon ${hide}`} alt="nav-icon" />
+                <img src={navIcon} id='nav-icon-2' className={`nav-icon ${hide}`} alt="nav-icon" />
+              </div>
+                <ul className={`nav-links || flex ${hide}`}>
                   <li><a href="#" className="nav-link">Programs</a></li>
                   <li><a href="#" className="nav-link">Pricing</a></li>
                   <li><a href="#" className="nav-link">Set Goals</a></li>
